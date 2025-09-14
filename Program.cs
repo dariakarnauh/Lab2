21b31d93337a6d2192ab6bce20ea7f1dc34105ad
dariakarnauh <dasakarnauh2@gmail.com>
2025-09-14 12:49:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.Design;
+
+Console.WriteLine("write score: ");
+string input = Console.ReadLine();
+if (!int.TryParse(input, out int score))
+{
+    Console.WriteLine("not that");
+}
+else
+{
+    if (score <= 60)
+    {
+        Console.WriteLine("not passed");
+    }
+    else
+    {
+        Console.WriteLine("passed)");
+    }
+}
