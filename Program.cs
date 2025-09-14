d563d4cfdd8ccea7743080e4dc7e8c1a2eb43083
dariakarnauh <dasakarnauh2@gmail.com>
2025-09-14 12:52:17 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿class Task4
+{
+    public static void Main()
+    {
+        string name = Console.ReadLine();
+        string input = Console.ReadLine();
+        bool isSuccsess = int.TryParse(input, out int numberInput);
+        string name2 = Console.ReadLine();
+        string input2 = Console.ReadLine();
+        bool isSuccsess2 = int.TryParse(input2, out int numberInput2);
+        if (numberInput > numberInput2)
+        {
+            Console.WriteLine("Student: " + name + " Top: " + numberInput);
+            return;
+        }
+        else if (numberInput < numberInput2)
+        {
+            Console.WriteLine("Student: " + name2 + " Top: " + numberInput2);
+            return;
+        }
+        else if (numberInput == numberInput2)
+        {
+            Console.WriteLine("draw: " + numberInput);
+            return;
+        }
+    }
+}
