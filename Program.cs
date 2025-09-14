5f93fdfc51ebe2bc4f3450f43cc0a02eaebd952c
dariakarnauh <dasakarnauh2@gmail.com>
2025-09-14 12:50:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿class Task_2
+{
+    public static void Main(string[] args)
+    {
+        string name = Console.ReadLine();
+        string input = Console.ReadLine();
+        bool isSuccess = int.TryParse(input, out int numberInput);
+        if (isSuccess)
+        {
+            if (numberInput > 60 && numberInput < 101)
+
+            {
+                Console.WriteLine(name + " Pass");
+            }
+            else
+            {
+                Console.WriteLine(name + " Don't pass");
+            }
+        }
+        else
+        {
+            Console.WriteLine("not that");
+        }
+        if (numberInput > 100)
+        {
+            Console.WriteLine("not that");
+        }
+;
+    }
+}
